ef2b69c6a550a77451d1155002a716dd77d207e1
devarinti02 <125158820+devarinti02@users.noreply.github.com>
2025-08-02 15:39:24 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,9 +22,10 @@ data "aws_subnets" "default" {
   }
 }
 
-resource "aws_eks_cluster" "game-2048" {
+# EKS Cluster
+resource "aws_eks_cluster" "game_2048" {
   name     = "game-2048"
-  role_arn = "arn:aws:iam::672965104327:role/eks-ec2"
+  role_arn = "arn:aws:iam::672965104327:role/eks-ec2" # Cluster role
   version  = "1.31"
 
   vpc_config {
@@ -34,20 +35,57 @@ resource "aws_eks_cluster" "game-2048" {
   access_config {
     authentication_mode = "API"
   }
+
+  depends_on = [aws_iam_role.eks_cluster_role]
 }
 
-resource "aws_eks_fargate_profile" "game_fargate_profile" {
-  cluster_name           = aws_eks_cluster.game-2048.name
-  fargate_profile_name   = "game-2048-fargate"
-  pod_execution_role_arn = "arn:aws:iam::672965104327:role/eks-fargate"
+# Node group role
+resource "aws_iam_role" "eks_nodegroup_role" {
+  name = "eks-nodegroup-role"
 
-  subnet_ids = data.aws_subnets.default.ids
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Effect = "Allow"
+      Sid    = ""
+    }]
+  })
+}
 
-  selector {
-    namespace = "default"
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  role       = aws_iam_role.eks_nodegroup_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  role       = aws_iam_role.eks_nodegroup_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  role       = aws_iam_role.eks_nodegroup_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+# EKS Node Group
+resource "aws_eks_node_group" "game_2048_nodes" {
+  cluster_name    = aws_eks_cluster.game_2048.name
+  node_group_name = "game-2048-nodes"
+  node_role_arn   = aws_iam_role.eks_nodegroup_role.arn
+  subnet_ids      = data.aws_subnets.default.ids
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
   }
 
-  depends_on = [
-    aws_eks_cluster.game-2048
-  ]
+  instance_types = ["t3.medium"]
+  ami_type       = "AL2_x86_64"
+
+  depends_on = [aws_eks_cluster.game_2048]
 }
