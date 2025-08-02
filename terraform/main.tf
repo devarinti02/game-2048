2482eb0f97aecdaab8c2c229aadde6f8d104cd9d
devarinti02 <125158820+devarinti02@users.noreply.github.com>
2025-08-02 15:14:57 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-    region = "ap-southeast-1"
+  region = "ap-southeast-1"
 }
 
 data "aws_vpc" "default" {
@@ -24,7 +24,7 @@ data "aws_subnets" "default" {
 
 resource "aws_eks_cluster" "game-2048" {
   name     = "game-2048"
-  role_arn = "arn:aws:iam::672965104327:role/eks-ec2" 
+  role_arn = "arn:aws:iam::672965104327:role/eks-ec2"
   version  = "1.31"
 
   vpc_config {
@@ -36,4 +36,18 @@ resource "aws_eks_cluster" "game-2048" {
   }
 }
 
+resource "aws_eks_fargate_profile" "game_fargate_profile" {
+  cluster_name           = aws_eks_cluster.game-2048.name
+  fargate_profile_name   = "game-2048-fargate"
+  pod_execution_role_arn = "arn:aws:iam::672965104327:role/eks-fargate"
 
+  subnet_ids = data.aws_subnets.default.ids
+
+  selector {
+    namespace = "default"
+  }
+
+  depends_on = [
+    aws_eks_cluster.game-2048
+  ]
+}
