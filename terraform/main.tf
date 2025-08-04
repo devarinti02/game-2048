1b2239608df0465532197d99604cbb5da968ea2d
devarinti02 <125158820+devarinti02@users.noreply.github.com>
2025-08-05 00:10:53 +0530

    Add files via upload

---
+++ terraform/main.tf
@@ -0,0 +1,74 @@
+############### this script helps to get details of default vpc and subnet id's data ##########################
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.0"
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+# Get default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get all subnets in default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# IAM role for EKS cluster
+resource "aws_iam_role" "eks_cluster" {
+  name = "eks-cluster-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+# Attach EKS cluster policy to the role
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# Create EKS cluster
+resource "aws_eks_cluster" "demo-cluster" {
+  name     = "demo-cluster"
+  role_arn = aws_iam_role.eks_cluster.arn
+  version  = "1.31"
+
+  vpc_config {
+    subnet_ids = data.aws_subnets.default.ids
+  }
+
+  access_config {
+    authentication_mode = "API"
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy
+  ]
+}
